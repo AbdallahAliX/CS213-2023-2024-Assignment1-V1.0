737dc93ef882ba87fa2ffc2ff4797ebee0c2cdee
Abdallah Mohamed Ali <abdallahali0004@gmail.com>
2023-10-05 16:22:36 +0300

    Init

---
+++ CS213-2023-20220185-20220405-20220305-A1-Part1.cpp
@@ -0,0 +1,65 @@
+// FCAI – OOP Programming – 2023 - Assignment 1 
+// Program Name:				CS213-2023-20220185-20220405-20220305-A1.cpp
+// Last Modification Date:	xx/xx/xxxx
+// Author1 and ID and Group:	Abdallah Ali 20220185
+// Author2 and ID and Group:	Youssef Mohamed 20220405
+// Author3 and ID and Group:	Mohamed Maged 20220305
+// Purpose: Adding filters to a gray scale image
+
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cmath>
+#include "bmplib.cpp"
+
+using namespace std;
+unsigned char image[SIZE][SIZE];
+
+void loadImage ();
+void saveImage ();
+void doSomethingForImage ();
+
+int main()
+{
+  loadImage();
+  doSomethingForImage();
+  saveImage();
+  return 0;
+}
+
+
+void loadImage () {
+   char imageFileName[100];
+
+   // Get gray scale image file name
+   cout << "Enter the source image file name: ";
+   cin >> imageFileName;
+
+   // Add to it .bmp extension and load image
+   strcat (imageFileName, ".bmp");
+   readGSBMP(imageFileName, image);
+}
+
+
+void saveImage () {
+   char imageFileName[100];
+
+   // Get gray scale image target file name
+   cout << "Enter the target image file name: ";
+   cin >> imageFileName;
+
+   // Add to it .bmp extension and load image
+   strcat (imageFileName, ".bmp");
+   writeGSBMP(imageFileName, image);
+}
+
+
+void doSomethingForImage() {
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j< SIZE; j++) {
+
+
+    }
+  }
+}
